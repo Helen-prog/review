585cfc8957cd949f45c8833f0c211c4be046c23e
Helen <elena.kozovyakina@gmail.com>
2022-07-11 14:58:48 +0300

    review

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import "./styles.css";
 
 function App() {
+  // State with list of all checked item
+  const [name, setName] = useState('');
+  const [checked, setChecked] = useState([]);
+  // const checkList = ["Apple", "Banana", "Tea", "Coffee"];
+  const checkList = {"Яблоко": "Apple", "Банан": "Banana", "Чай": "Tea", "Кофе": "Coffee"};
+
+  // Add/Remove checked item from list
+  const handleCheck = (event) => {
+    var updatedList = [...checked];
+    if (event.target.checked) {
+      updatedList = [...checked, event.target.value];
+    } else {
+      updatedList.splice(checked.indexOf(event.target.value), 1);
+    }
+    setChecked(updatedList);
+  };
+
+  // Generate string of checked items
+  const checkedItems = checked.length
+    ? checked.reduce((total, item) => {
+        return total + ", " + item;
+      })
+    : "";
+
+  // Return classes based on whether item is checked
+  var isChecked = (item) =>
+    checked.includes(item) ? "checked-item" : "not-checked-item";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="checkList">
+        <input value={name}  onChange={e => setName(e.target.value)} placeholder="Имя" />
+        <div className="list-container">
+          {Object.keys(checkList).map((item, index) => (
+            <div key={index}>
+              <input value={item} type="checkbox" onChange={handleCheck} />
+              <span className={isChecked(item)} title={item}>{checkList[item]}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="otziv">
+        {`${name}, ${checkedItems}`}
+      </div>
     </div>
   );
 }
